48e54ee09da65d77fbcc5c421e48d6d2bd36117b
Sergio Correia <sergio.correia@gmail.com>
2017-03-08 14:23:41 -0500

    Filter that fixes TOC with many separate docs

---
+++ filters/fixtoc.py
@@ -0,0 +1,40 @@
+"""Create table of contents from multiple files
+
+Usage:
+
+    pandoc --number-sections --file-scope --toc -s *.md | pandoc -f html -o toc.html -F fixtoc.py -M files:"*.md"
+"""
+
+import glob
+import panflute as pf
+
+
+def prepare(doc):
+    # Create file list
+    fns = doc.get_metadata('files')
+    pf.debug('-' * 64)
+    pf.debug('Expanding pattern:', fns)
+    fns = glob.glob(fns)
+    pf.debug('Files:', fns)
+    fns = [fn.replace('.md', '.html') for fn in fns]    
+    doc.fns = fns
+    pf.debug('-' * 64)
+
+    # Clear all contents except TOC
+    doc.content.list = doc.content.list[:1]
+
+
+def action(e, doc):
+    if isinstance(e, pf.Div) and e.identifier=='TOC':
+        e.walk(fix_links)
+
+
+def fix_links(e, doc):
+    if isinstance(e, pf.Link):
+        e.url = doc.fns[0] + e.url
+    elif isinstance(e, pf.ListItem) and isinstance(e.parent.parent, pf.Div):
+        doc.fns.pop(0)  # Switch to the next filename
+
+
+if __name__ == '__main__':
+    pf.run_filter(action, prepare=prepare) 
